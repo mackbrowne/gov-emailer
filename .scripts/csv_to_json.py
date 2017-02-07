28d78fad5698bc508d3f67c341a890148c111e39
danielsnider <danielsnider12@gmail.com>
2017-02-07 12:46:09 -0800

    Removed names with accents because they result in invalid email addresses

--- .scripts/csv_to_json.py
+++ .scripts/csv_to_json.py
@@ -1,7 +1,8 @@
 # Transform the parliment.csv to json in the format that we want
+#
+# parliment.csv comes from: http://www.parl.gc.ca/Parliamentarians/en/members
 
 #TODO(Dan): Replace weird dashes in CSV to normal dashes, otherwise the weird dashes introduce \u2014 chars
-#TODO(Dan): Look for spaces in email addresses
 #TODO(Dan): Look for other data quality issues
 
 import json
@@ -12,12 +13,19 @@ df = df.rename(index=str, columns={"Province / Territory": "province", "First Na
 
 df["email"] = df["firstName"]+ '.' + df["lastName"] + '@parl.gc.ca'
 
+# Find courner cases (for experimenting)
+# df[df['email'].str.contains('.\..\.')] # T.J. Harvey, you bastard
+
+# Fix courner cases
+df['email'] = df['email'].str.replace(' ', '') # Remove spaces in names
+df['email'] = df['email'].str.replace('(.)(\.)(.)(\.)', '\\1\\3') # Fix first names that look like T.J. (change to TJ)
+df['email'] = df['email'].str.replace('.\.\.', '.') # Remove abbreviation in name such as 'A.'
 
 json_data = df.to_json(orient='records')
 
 ## If you want to experiment with the data, load the json from str format to dict
-obj = json.loads(json_data)
-obj[0]
+# obj = json.loads(json_data)
+# obj[0]
 
 file = open('../public/parliment.json', 'wb')
 file.write(json_data)
