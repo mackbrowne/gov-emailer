c5202dad852dca31c09268b2fa4f93b3ae4bac91
Mack Browne <mackbrowne@gmail.com>
2017-02-07 15:50:01 -0500

    75% complete mvp functionality

--- imports/ui/components/home/home.js
+++ imports/ui/components/home/home.js
@@ -19,38 +19,74 @@ class Home {
 
     this.mps = mpService.mps;
 
-    this.provinces = provinceService.provinces;
+    this.provinces = provinceService.provinces.map((province) => {
+      return {
+        name: province,
+        selected: false
+      }
+    });
+
+    let parties = [
+      'liberal',
+      'ndp',
+      'conservative',
+      'green'
+    ];
+
+    this.selectedParties = parties.map((party) => {
+      return {
+        name: party,
+        selected: false
+      }
+    });
 
     // Agency Theme JavaScript
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 54)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 54)
+      }, 1250, 'easeInOutExpo');
+      event.preventDefault();
     });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
-        target: '#mainNav',
-        offset: 54
+      target: '#mainNav',
+      offset: 54
     });
 
     // Closes the Responsive Menu on Menu Item Click
     $('#navbarResponsive>ul>li>a').click(function() {
-        $('#navbarResponsive').collapse('hide');
+      $('#navbarResponsive').collapse('hide');
     });
 
     // jQuery to collapse the navbar on scroll
     $(window).scroll(function() {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
     });
 
+  }
+
+  select(item) {
+    item.selected = !item.selected;
+  }
+
+  selectAll(list) {
+    list.forEach((item) => {
+      this.select(item)
+    });
+  }
+
+  emailNow() {
+    // this.mps.filter((mp)=>{
+    //
+    // });
+    // debugger;
   }
 }
 
@@ -58,14 +94,14 @@ const name = 'home';
 
 // create a module
 export default angular.module(name, [
-  angularMeteor,
-  uiRouter
-]).component(name, {
-  template,
-  controllerAs: name,
-  controller: Home
-})
-.config(config);
+    angularMeteor,
+    uiRouter
+  ]).component(name, {
+    template,
+    controllerAs: name,
+    controller: Home
+  })
+  .config(config);
 
 function config($stateProvider) {
   'ngInject';
