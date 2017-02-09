bd385abc9c3fd7839f49380a206250a534d20ea0
nisten <n.tahiraj@gmail.com>
2017-02-09 15:07:02 -0500

    Added an extra step with text input box and 2 way angular binding

--- client/main.js
+++ client/main.js
@@ -14,7 +14,38 @@ function onReady() {
   ], {
     strictDi: true
   });
+  // Agency Theme JavaScript
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 54)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Closes the Responsive Menu on Menu Item Click
+  $('#navbarResponsive>ul>li>a').click(function() {
+    $('#navbarResponsive').collapse('hide');
+  });
+
+  // jQuery to collapse the navbar on scroll
+  $(window).scroll(function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  });
+
 }
+
 
 if (Meteor.isCordova) {
   angular.element(document).on('deviceready', onReady);
--- imports/ui/components/home/home.js
+++ imports/ui/components/home/home.js
@@ -20,6 +20,11 @@ class Home {
     this.mps = mpService.mps;
     this.selectedMPS = [];
 
+
+    this.derp = "Meow" ;
+
+
+
     this.provinces = provinceService.provinces.map((province) => {
       return {
         name: province,
@@ -41,35 +46,7 @@ class Home {
       }
     });
 
-    // Agency Theme JavaScript
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-        scrollTop: ($($anchor.attr('href')).offset().top - 54)
-      }, 1250, 'easeInOutExpo');
-      event.preventDefault();
-    });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-      target: '#mainNav',
-      offset: 54
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('#navbarResponsive>ul>li>a').click(function() {
-      $('#navbarResponsive').collapse('hide');
-    });
-
-    // jQuery to collapse the navbar on scroll
-    $(window).scroll(function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-      } else {
-        $("#mainNav").removeClass("navbar-shrink");
-      }
-    });
 
   }
 
